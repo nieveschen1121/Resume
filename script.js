001f14a6aa882a9442d13007ac335f93ead4d591
Nieves <nieveschen1121@gmail.com>
2019-10-13 23:36:20 +0800

    Create page.

---
+++ script.js
@@ -0,0 +1,221 @@
+var resume = {
+  displayType: "profile",
+  profile: {
+    name: "陳亭秀",
+    application: "課程行銷",
+    introduction: "HI, 我是亭秀, 從事行銷、業務兩年,具備開發市場、廣告投放、SEO 優化能力，過去曾擁有創立新廠提升公司 25 % 總業績、獨立爭取 320 萬專案訂單、㇐年以上臉書粉專經營等經驗，「It's kind of fun to do the impossible」是我的生活態度, 我喜歡大膽挑戰、謹慎執行, 樂於解決問題、多元學習。曾困擾於跨領域學習資源取得不易、目睹教育現場的無奈, 於是更認同 YOTTA 運用科技傳遞知識的理念, 期許能參與行銷團隊, 在其中貢獻所有自身的經驗、技能與資源, 為更理想的教育環境盡㇐己之力。",
+    mobile: "+886 985639168",
+    email: "nieveschen1121@gmail.com"
+  },
+  experiences: [
+    [
+      {
+        organization: "萬泰水塔高雄廠",
+        position: "創廠人、經理",
+        duration: {
+          beginYYYY: 2018,
+          beginMM: 7,
+          endYYYY: 2019,
+          endMM: 6
+        },
+        description: "工廠選址、廠商簽約、人員訓練、財務管理、業務拜訪、客訴處理、數據分析、廣告投放、粉專經營",
+        items: [
+          { description: "僅用 8 萬的起始成本創廠，營業兩月後打平收支開始獲利" },
+          { description: "獨立洽談 320 萬的專案訂單成功" },
+          { description: "提升公司總營業額 25%" },
+          { description: "培育 4 位新人，穩定公司制度" },
+        ]
+      },
+    ],
+    [
+      {
+        organization: "萬泰水塔股份有限公司",
+        position: "行銷業務人員",
+        duration: {
+          beginYYYY: 2017,
+          beginMM: 9,
+          endYYYY: 2018,
+          endMM: 6
+        },
+        description: "廣告投放、SEO、電話及EDM行銷、業務拜訪、粉專經營、客服諮詢、物流安排、商務差旅",
+        items: [
+          { description: "擴大經營電子商務吸收 B2C 市場、維繫原本的 B2B 客群" },
+          { description: "獨創立 Facebook 粉專、GMB 提高品牌曝光率" },
+          { description: "學習 SEO 優化網頁讓水塔關鍵字排名保持在第一頁" },
+          { description: "2017 年至吉隆坡、亞庇拜訪外銷客戶、市場調查" },
+          { description: "2018 年至福島洽談機械手臂之運用、方形蓄水容器合作" },
+        ]
+      },
+    ],
+    [
+      {
+        organization: "人才天下有限公司(兼職)",
+        position: "舞台引導、貴賓接待",
+        duration: {
+          beginYYYY: 2019,
+          beginMM: "",
+          endYYYY: "",
+          endMM: ""
+        },
+        description: "",
+        items: [
+          { description: "SEMICON Taiwan 國際半導體展-舞台引導、貴賓接待" },
+        ]
+      },
+      {
+        organization: "品漾創意行銷公司(兼職)",
+        position: "主持人、活動人員",
+        duration: {
+          beginYYYY: 2017,
+          beginMM: "",
+          endYYYY: 2019,
+          endMM: ""
+        },
+        description: "",
+        items: [
+          { description: "交通部觀光局台灣好行祈福之旅-活動主持人" },
+          { description: "高雄市、台南市交通局記者會-活動組" },
+          { description: "嘉義市新港鄉公所農產節-活動組" },
+        ]
+      },
+      {
+        organization: "騰風國際整合行銷有限公司(兼職)",
+        position: "活動人員",
+        duration: {
+          beginYYYY: 2016,
+          beginMM: "",
+          endYYYY: 2017,
+          endMM: ""
+        },
+        description: "",
+        items: [
+          { description: "高雄市觀光局冬季國際旅展-活動主持、行銷組" },
+          { description: "華科事業群、日月光測試廠旺年會-活動組、公關組" },
+        ]
+      },
+    ],
+    [
+      {
+        organization: "文藻外語大學",
+        position: "主修西文 副修英文 選修印尼",
+        duration: {
+          beginYYYY: 2011,
+          beginMM: 9,
+          endYYYY: 2017,
+          endMM: 6
+        },
+        description: "",
+        items: [
+          { description: "2016 年師長推薦至印尼雅加達參加東南亞協會台商研習團" },
+          { description: "2014 年代表學校至印尼 Petra 大學進行全球學生交流計畫" },
+          { description: "文藻外語大學西文科第四十六屆畢業公演–舞台總監" },
+          { description: "文藻外語大學小提琴社第九屆活動、第十屆社長" },
+        ]
+      }
+    ],
+  ],
+  skills: {
+    language: [
+      {
+        name: "中文",
+        level: 3
+      },
+      {
+        name: "台語",
+        level: 3
+      },
+      {
+        name: "英文",
+        level: 2
+      },
+      {
+        name: "西班牙文",
+        level: 2
+      },
+      {
+        name: "印尼文",
+        level: 1
+      },
+      {
+        name: "日文",
+        level: 1
+      },
+    ],
+    computer: [
+      { name: "MS Office" },
+      { name: "MS Power BI" },
+      { name: "Adobe Illustrator" },
+      { name: "Adobe Photoshop" },
+    ],
+  },
+  certificates: [
+    { name: "BULATS 國際職場英語檢定 B1" },
+    { name: "BULATS 國際職場西語檢定 B1" },
+    { name: "資策會 SEO 網路行銷曝光實務" },
+  ],
+  autobiography: {
+    displayPage: 1,
+
+    background: {
+      title: "成長背景與學習歷程",
+      content: "\
+<p>我是陳亭秀，來自台南。由於父親從商，在耳濡目染之下，自小喜歡幫忙做生意與人接觸，喜歡上經營獲得的成就感；母親是國中教師，總是教導我多方學習與不吝分享，培養出我多元學習的熱情與待人處世的原則。</p>\
+<p>在學時期主修西班牙文、副修英文皆取得 BULATS 檢定 B1 證照。<br>\
+2014 年代表學校至印尼 Petra 大學進行暑期全球學生交流計畫選修印尼文。並於 2016 經師長推薦至印尼雅加達參加東南亞協會台商研習團。<br>\
+多次出訪的經驗讓我獲益良多，也深信學習多國語言是為了更深入地把話說進對方的心裡，進而獲得更多交流的機會。</p>\
+<p>求學時期由於我善於人際互動，又充滿鬼點子的個性受社員青睞，於小提琴社擔任數屆活動長及社長；在任時期我與社團夥伴積極參與曝光活動、爭取商演機會，因而使人數、經費突破皆歷屆最高紀錄。帶領社團首屆參加全國音樂競賽獲得甲等佳績，更代表學校參加全國大專院校社團評鑑取得優等殊榮。</p>\
+<p>課餘時間我安排打工提前進入職場學習，曾擔任櫃台、餐廳內外場；也曾參與過行銷活動規劃、商業午餐企劃。<br>\
+我曾於品漾創意行銷公司、騰風國際整合行銷公司擔任主持人及活動人員，並受老闆肯定持續合作至今。抱持著把事情做到完美的信念，我將每一份工作都當作是自己的事業在經營；即便離職後，我也時時感謝老闆們對我的照顧、提攜，與他們時常聯絡、互相關心。<br>\
+透過這些經驗，發掘自己對行銷工作的熱情、從中獲得成就感，也確立了未來發展方向。</p>\
+<p>此外，我認為工作與生活要相輔相成，才能使維持身心平衡，維持最佳狀態。<br>\
+工作之餘我的興趣是旅行、潛水、滑雪、空中瑜珈、攀岩、烘焙、裱花…等活動，也參與過標準鐵人三項接力賽、泳渡日月潭及登頂玉山，不但可以訓練自己的韌性，還可以結交各行各業的人脈拓展眼界甚至是改變自己，最重要的是保持學習的習慣在人生路上持續進修。</p>"
+    },
+    others: [
+      {
+        title: "專業經歷",
+        content: "\
+<p>2017 年自文藻外語大學西班牙文科畢業後進入職場，擔任行銷人員，觀察到傳統產業公司應該要轉型，必須開始經營電子商務，擴大B2C市場也得維繫原本的 B2B 客群。<\p>\
+<p>為了說服老闆提供更多資源，我創立了 FB 粉絲專頁自掏腰包投放廣告，也學習 SEO 優化現有網頁、投放 GOOGLE ADS PPC 廣告，讓水塔關鍵字搜尋排名保持在第一頁；並分析客戶來源證明網路客群日益龐大，成功爭取到公司投放資源。<\p>\
+<p>2017 年出差至西馬吉隆坡拜訪外銷客戶、市場調查，同年也出差至東馬進行市場調查、廠地觀摩。2018 年出差至日本福島參訪機械手臂之運用及方形蓄水容器合作洽談。藉由涉獵不同產業與不斷強化既有經驗，協助公司在工業技術上升級。</p>\
+<p>隔年升任經理，由於過去一年以來發覺高屏市場利潤偏高但要求品質及即時服務，便爭取老闆支持創立萬泰水塔高雄廠。<br>\
+起初老闆投資意願不高、提供資源有限。從工廠選址、廠商簽約、人員訓練到開業後業務拜訪、客訴處理、數據分析皆由我親力親為，僅用 8 萬的起始成本，使據點營業兩月後打平收支、第三個月開始獲利並於同月獨立拿下 320 萬的專案訂單；至今據點年營收占總公司營業額 25%。<\p>\
+<p>任職經理期間共培育四位新人，因為曾任職基層，更能體會他們的心聲。我運用先同理、再溝通、後決策的管理方式，帶領團隊具有熱情，更能從工作中獲得成就創造雙贏。於公司穩定獲利、育才計畫完成後交接並離職結束兩年的服務。<\p>"
+      },
+      {
+        title: "未來展望",
+        content: "離職之後我自學 Illustrator, PowerBi 軟體工具精進自身能力，當前目標是希望帶著傳統產業的行銷、業務實戰經驗前往志趣相投的新創產業，讓自己有機會貢獻經驗與資源持續成長、也想回饋社會賦予工作不同的意義。"
+      }
+    ]
+  }
+}
+var levelLookup = {
+  "1": "基礎",
+  "2": "中等",
+  "3": "流利"
+};
+
+var vm = new Vue({
+  el: "#app",
+  data: resume,
+  methods: {
+    toTop: function (e) {
+      $("html, body").animate({ scrollTop: 0 }, 300);
+    },
+    // switchPage: function (e) {
+    //   let $button = $(e.target);
+    //   $button.parent(".nav").find(".pageButton").removeClass("selected");
+    //   $button.addClass("selected");
+    //   this.autobiography.displayPage = parseInt($button.attr("page"));
+    // },
+    switchDisplayType: function (e) {
+      if (this.displayType === "profile") {
+        this.displayType = "autobiography";
+      }
+      else {
+        this.displayType = "profile";
+      }
+
+      this.toTop(e);
+    }
+  }
+});
